8dda8580dbc6e3aa7978bbeafb123fe900595905
Verena Mueller <verena.mller@gmail.com>
2020-02-24 18:11:35 +0100

    Spot Views Index + Show

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,7 +1,3 @@
 class Spot < ApplicationRecord
-  belongs_to :user
-  validates :title, presence: true, uniqueness: true
-  validates :description, presence: true
-  validates :price, presence: true
-  validates :title, presence: true, default: true
+
 end
