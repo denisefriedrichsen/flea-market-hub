53e1db38aee4da6bd2dd5ee456755d7041dd4157
Verena Mueller <verena.mller@gmail.com>
2020-02-25 18:56:32 +0100

    add user account to navbar

--- db/schema.rb
+++ db/schema.rb
@@ -39,7 +39,7 @@ ActiveRecord::Schema.define(version: 2020_02_24_160637) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "role"
+    t.boolean "admin", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
