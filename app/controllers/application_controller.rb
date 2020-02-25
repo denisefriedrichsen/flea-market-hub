98f862ef230cf66d38d41653550cdc4e17565541
Verena Müller <59503846+Vronski13@users.noreply.github.com>
2020-02-25 12:54:28 +0100

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,16 +8,16 @@ class ApplicationController < ActionController::Base
   end
 
 
-  # include Pundit
+  include Pundit
 
   # # Pundit: white-list approach.
-  # after_action :verify_authorized, except: :index, unless: :skip_pundit?
-  # after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
+  after_action :verify_authorized, except: :index, unless: :skip_pundit?
+  after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
 
-  # private
+  private
 
-  # def skip_pundit?
-  #   devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
-  # end
+  def skip_pundit?
+    devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
+  end
 end
 
