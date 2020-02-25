19a5f5c93e9c2ceceb940bb5290c55b320680c49
Flo Leinenkugel <flolein@gmx.net>
2020-02-25 16:27:51 +0100

    created Spots #create und #destroy, bug fixes in Spots controller, spots policy, spots form, spots new, spots show, routes

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -19,9 +19,9 @@ class SpotsController < ApplicationController
     @spot = Spot.new(spot_params)
     authorize @spot
     @spot.availability == true
-    @spot.user_id == current_user
+    @spot.user = current_user
     if @spot.save
-      redirect_to spot_path(@spot)
+      redirect_to @spot
     else
       render 'new'
     end
@@ -38,6 +38,6 @@ class SpotsController < ApplicationController
   private
 
   def spot_params
-    params.require(:spot).permit(:title, :price, :description, :user_id)
+    params.require(:spot).permit(:title, :price, :description)
   end
 end
