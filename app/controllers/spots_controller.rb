926580403f9019d6820fd7b2b35afab461c6c4b4
Marian Bohndick <marian.bohndick@gmail.com>
2020-02-25 12:46:02 +0100

    started authorization

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -2,18 +2,22 @@ class SpotsController < ApplicationController
 
   def index
     @spots = Spot.all
+    # authorize @spots
   end
 
   def show
     @spot = Spot.find(params[:id])
+    # authorize @spot
   end
 
   def new
     @spot = Spot.new
+    # authorize @spot
   end
 
   def create
     @spot = Spot.new(spot_params)
+    # authorize @spot
     if @spot.save
       redirect_to spot_path(@spot)
     else
---
+++ app/policies/spot_policy.rb
@@ -0,0 +1,24 @@
+class SpotPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+
+    def index?
+      return true
+    end
+
+    def show?
+      return true
+    end
+
+    def new?
+      return true
+    end
+
+    def create?
+      return true
+    end
+
+  end
+end
