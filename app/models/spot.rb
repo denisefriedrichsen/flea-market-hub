0fb2fca961d843bb0c8ea5e1d7014682147663a7
denise friedrichsen <denise.friedrichsen@yahoo.de>
2020-02-27 12:26:46 +0100

    Added new columns to spot

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,11 +1,11 @@
 class Spot < ApplicationRecord
   has_one_attached :photo
-
   has_many :bookings, dependent: :destroy
   belongs_to :user
   validates :title, presence: true
   validates :price, presence: true
   validates :availability, presence: true, inclusion: { in: [true, false] }
   validates :user_id, presence: true
-  validates :photo, presence: true
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_26_105911) do
+ActiveRecord::Schema.define(version: 2020_02_27_105202) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,17 +51,20 @@ ActiveRecord::Schema.define(version: 2020_02_26_105911) do
     t.string "title"
     t.text "description"
     t.integer "price"
-    t.boolean "availability", default: true
+    t.boolean "availability"
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "status", default: "available"
+    t.float "latitude"
+    t.float "longitude"
+    t.string "address"
     t.index ["user_id"], name: "index_spots_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.boolean "admin", default: false
+    t.string "role"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
