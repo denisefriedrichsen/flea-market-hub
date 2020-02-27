bd8415d91dbdbe891584c6e5b831a33e5cace7b4
denise friedrichsen <denise.friedrichsen@yahoo.de>
2020-02-27 20:43:03 +0100

    Added new css to the homepage

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -8,12 +8,12 @@ class Spot < ApplicationRecord
   validates :user_id, presence: true
   validates :photo, presence: true
 
-  # include PgSearch::Model
-  # pg_search_scope :search_by_title_and_description,
-  #   against: [ :title, :description ],
-  #   using: {
-  #     tsearch: { prefix: true }
-  #   }
+  include PgSearch::Model
+  pg_search_scope (:search_by_title_and_description,
+    against: [:title, :description],
+    using: {
+      tsearch: { prefix: true }
+    }
 
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
