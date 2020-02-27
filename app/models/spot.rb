8be5123b2aea3559083c4d770863f8f00d3b863e
Verena Müller <59503846+Vronski13@users.noreply.github.com>
2020-02-27 12:52:02 +0100

    Update spot.rb

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -3,6 +3,7 @@ class Spot < ApplicationRecord
   has_many :bookings, dependent: :destroy
   belongs_to :user
   validates :title, presence: true
+  validates :photo, presence: true
   validates :price, presence: true
   validates :availability, presence: true, inclusion: { in: [true, false] }
   validates :user_id, presence: true
